abadf0f3226fd772178985dd1200d3c859bb5519
Srikara S B <srikara.sb@robosoftin.com>
2021-07-17 15:34:13 +0530

    configured Localizaton

--- AspireApp/src/containers/ComingSoon/ComingSoon.js
+++ AspireApp/src/containers/ComingSoon/ComingSoon.js
@@ -5,12 +5,13 @@ import {View, Text} from 'react-native';
 import {Actions} from '../../redux';
 import Logo from '../../assets/images/logos/Logo.svg';
 import {styles} from './styles';
+import {Constants} from '../../localization';
 
 const ComingSoon = function (props) {
   return (
     <View style={styles.container}>
       <Logo width="200" height="100" />
-      <Text style={styles.comingSoon}>Coming Soon</Text>
+      <Text style={styles.comingSoon}>{Constants.comingSoon}</Text>
     </View>
   );
 };
---
+++ AspireApp/src/localization/index.js
@@ -0,0 +1,5 @@
+import LocalizedStrings from 'react-localization';
+
+export const Constants = new LocalizedStrings({
+  en: require('./i18n/en.json'),
+});

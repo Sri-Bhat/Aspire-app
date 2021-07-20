3fae98ecad68422e61596d13914a425dc392300b
Srikara S B <srikara.sb@robosoftin.com>
2021-07-20 23:06:09 +0530

    Added Fix

--- AspireApp/src/utils/commonStyles.js
+++ AspireApp/src/utils/commonStyles.js
@@ -2,16 +2,16 @@ import {Colors} from '../assets/colors';
 import Scaling from './scaling';
 
 // This is the common styles which are normally used in multiple times in our code
-export const CommonStyles = {
-  boldText: {
+export default class CommonStyles {
+  static boldText = {
     fontSize: Scaling.ms(24),
     fontWeight: 'bold',
     color: Colors.white,
-  },
+  };
 
-  container: {
+  static container = {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-  },
-};
+  };
+}
